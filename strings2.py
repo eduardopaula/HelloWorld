eb484c4e6316575674d3d5050a29802b33f119b4
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2023-12-14 12:47:18 -0300

    Fatiando Strings com números negativos e steps

--- strings2.py
+++ strings2.py
@@ -1,14 +1,19 @@
-# 1
+letters = "abcdefghijklmnopqrstuvwxyz"
 numeros = "012345678901234"
-
 parrot = "Norwegian Blue"
-
 print(parrot)
 
 # print(parrot[0:6])  # Norweg
 # print(parrot[-14:-8])
-#
-print(parrot[-4:-2])    # Bl
-print(parrot[-4:12])    # Bl
+# print(parrot[-4:-2])    # Bl
+# print(parrot[-4:12])    # Bl
+# print(parrot[-3:14])    # Bl
+print(parrot[0:6:2])    # Nre
+print(parrot[0:6:3])    # Nw
 
-# letters = "abcdefghijklmnopqrstuvwxyz"
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
