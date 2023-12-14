5029710edb37ed05d95ed5c117fcdf7a96a9042a
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2023-12-14 11:27:37 -0300

    Fatiando Strings 2

--- strings2.py
+++ strings2.py
@@ -1,20 +1,22 @@
 # 1
-#  012345678901234
+numeros = "012345678901234"
 
 parrot = "Norwegian Blue"
 
 print(parrot)
-# print(parrot[3])
-# print(parrot[4])
-# print(parrot[9])
-# print(parrot[3])
-# print(parrot[6])
-# print(parrot[8])
-# print(parrot[0])
-# print()
-# print(len(parrot))
-# print(parrot[-11])
-print(parrot[-1])
-print(parrot[-2])
-print(parrot[-3])
-print(parrot[13 - 14])
+# print(parrot[0:6])
+# print(parrot[3:5])
+# print(parrot[0:9])
+# print(parrot[:9])
+# print(parrot[10:14])
+
+# print(parrot[10:])
+#
+# print(parrot[:6])
+# print(parrot[6:])
+# #
+# print(parrot[:6] + parrot[6:])
+#
+print(parrot[:])
+#
+# letters = "abcdefghijklmnopqrstuvwxyz"
