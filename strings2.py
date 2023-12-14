f5b95a5a2f1422d066a2a01975f1e875b21958fe
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2023-12-14 11:37:54 -0300

    Fatiando Strings com números negativos

--- strings2.py
+++ strings2.py
@@ -4,19 +4,11 @@ numeros = "012345678901234"
 parrot = "Norwegian Blue"
 
 print(parrot)
-# print(parrot[0:6])
-# print(parrot[3:5])
-# print(parrot[0:9])
-# print(parrot[:9])
-# print(parrot[10:14])
 
-# print(parrot[10:])
+# print(parrot[0:6])  # Norweg
+# print(parrot[-14:-8])
 #
-# print(parrot[:6])
-# print(parrot[6:])
-# #
-# print(parrot[:6] + parrot[6:])
-#
-print(parrot[:])
-#
+print(parrot[-4:-2])    # Bl
+print(parrot[-4:12])    # Bl
+
 # letters = "abcdefghijklmnopqrstuvwxyz"
