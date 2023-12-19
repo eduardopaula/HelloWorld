b0bda26d0bf7c49369c7a3fb4e2b44fcebb75cc9
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2023-12-19 14:25:35 -0300

    Aula 67

---
+++ adventure.py
@@ -0,0 +1,7 @@
+available_exists = ["north", "south", "east", "west"]
+
+chosen_exit = ""
+while chosen_exit not in available_exists:
+    chosen_exit = input("Please choose a direction")
+
+print("aren't you glad you got out of there")
